4627f7e0e0901e64ec798caf78c1de1c276f993b
令 <120230459+z466599850@users.noreply.github.com>
2023-02-25 22:20:38 +0800

    Add files via upload

---
+++ day_2023_2_24/HelloJava.java
@@ -0,0 +1,16 @@
+/**
+	@author wangling
+	@version v1.0
+	这是我的第一个java程序！非常的开心
+*/
+public class HelloJava
+{
+	//如下的main方法是程序的入口！
+	public static void main(String[] args)
+	{
+		//如下的语句表示输出到控制台
+		//println 一行的意思
+		System.out.println("HelloJava!");
+	}
+}
+
